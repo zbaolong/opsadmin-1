e2bc3dda41026009a28ec5df0eff5c8f686b4ecc
iluoy <376419847@qq.com>
2018-10-03 01:47:47 +0800

    设计模型

---
+++ opsadmin/asset/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+class Host(models.Model):
+    os_platform = models.
--- opsadmin/client/cmdb-client/bin/cmdb_client.py
+++ opsadmin/client/cmdb-client/bin/cmdb_client.py
@@ -6,46 +6,82 @@ import subprocess
 import platform
 import json
 import requests
+import socket
+import uuid
+import psutil
 
+def get_mac():
+    ret = {}
+    result=uuid.UUID(int = uuid.getnode()).hex[-12:]
+    ret['mac'] = ":".join([result[e:e+2] for e in range(0,11,2)])
+    return ret
+
+def get_hostname():
+    ret = {}
+    hostname = socket.gethostname()
+    ret['hostname'] = hostname
+    return ret
+
+def get_ip():
+    ret = {}
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+	ret['outer_ip'] = s.getsockname()[0]
+    finally:
+        s.close()
+    return ret
 
 def get_os_info():
-    os_info = {}
-    ret = platform.platform().split('-')
-    os_info['os_platform'] = ret[0]
-    os_info['os_type'] = ret[5]
-    os_info['os_kernel'] = ret[1]
-    return os_info
+    ret = {}
+    result = platform.platform().split('-')
+    ret['os_platform'] = result[0]
+    ret['os_type'] = result[5]
+    ret['os_version'] = result[-2]
+    ret['os_kernel'] = result[1]
+    return ret
 
 def get_cpu_info():
-    cpu_info = {}
+    ret = {}
     cpu_arch_cmd = "lscpu | grep Architecture | awk '{print $2}'"
-    cpu_info['cpu_arch'] = subprocess.check_output(cpu_arch_cmd,shell=True).strip('\n')
-    cpu_num_cmd = "lscpu | grep ^CPU\(s\) | awk '{print $2}'"
-    cpu_info['cpu_num'] = subprocess.check_output(cpu_num_cmd,shell=True).strip('\n')
-    return cpu_info
+    ret['cpu_arch'] = subprocess.check_output(cpu_arch_cmd,shell=True).strip('\n')
+    ret['cpu_physical_num'] = psutil.cpu_count(logical=False)
+    ret['cpu_logical_num'] = psutil.cpu_count()
+    return ret
 
 def get_mem_info():
-    mem_info = {}
+    ret = {}
     mem_total_cmd = "cat /proc/meminfo  | grep MemTotal | awk '{print $2}'"
-    mem_info['mem_total'] = round(float(subprocess.check_output(mem_total_cmd,shell=True).strip('\n')) / 1024 /1024,2)
-    return mem_info
+    ret['mem_total'] = round(float(subprocess.check_output(mem_total_cmd,shell=True).strip('\n')) / 1024 /1024,2)
+    return ret
 
 def asset_info():
     asset_data = {}
-    asset_data['os_info'] = get_os_info()
-    asset_data['cpu_info'] = get_cpu_info()
-    asset_data['mem_info'] = get_mem_info()
+    hostname = get_hostname()
+    mac = get_mac()
+    outer_ip = get_ip()
+    os_info = get_os_info()
+    cpu_info = get_cpu_info()
+    mem_info = get_mem_info()
+    asset_data.update(hostname)
+    asset_data.update(mac)
+    asset_data.update(outer_ip)
+    asset_data.update(os_info)
+    asset_data.update(cpu_info)
+    asset_data.update(mem_info)
     return asset_data
 
 def run():
     data = asset_info()
-    requests.post(
-	    url="",
-            data = data,
+    r = requests.post(
+	    url="http://192.168.2.107:9090/add_host/",
+            json = json.dumps(data),
 	)
+    print(r)
     
 
 
 if __name__ == '__main__':
     run()
+    #print(asset_info())
 
