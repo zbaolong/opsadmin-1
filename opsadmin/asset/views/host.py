ba1a4089b047a05a5cc96a7844df22d54e5c7f71
iluoy <376419847@qq.com>
2018-10-11 00:07:21 +0800

    添加分页

--- opsadmin/asset/views/host.py
+++ opsadmin/asset/views/host.py
@@ -3,9 +3,20 @@
 
 from django.shortcuts import render
 from asset.models import Host
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 def show_hosts(request):
+    curren_page = request.GET.get('p')
+    if not curren_page:
+        curren_page = 1
     host_obj = Host.objects.all()
+    paginator = Paginator(host_obj,1)
+    try:
+        posts = paginator.page(curren_page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,'asset/host.html',locals())
 
 def show_detail_host(request):
